9c0e30665610d7036288aec28ae21f016259d92c
hanifazhar <azharhanif04@gmail.com>
2025-01-08 22:51:40 +0700

    final navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,37 @@
 import React from "react";
-import hamburger_active from "../assets/hamburger-active.svg"
-import hamburger_non_active from "../assets/hamburger-non-active.svg"
+import { FiMenu } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 import { useState } from "react"
 const Navbar = () => {
 
   const [toogleNavbar, setToogleNavbar] = useState(false);
 
   return (
-    <div>
-      <nav className="flex justify-between px-8 py-4">
+    <div className="bg-backgroundColor">
+      <nav className="flex items-center justify-between px-8 py-4 bg-backgroundColor md:bg-backgroundColor md:px-12 md:py-6 lg:bg-backgroundColor lg:w-[80%] mx-auto lg:px-0">
         <div>
-          <h1 className="font-bold">Azhar<span className="text-red-600 font-bold">Hanif</span></h1>
+          <h1 className="font-bold text-xl text-fontColor">Azhar<span className="font-bold text-mainColor">Hanif</span></h1>
         </div>
         <div 
-          className="cursor-pointer"
+          className="cursor-pointer md:hidden lg:hidden"
           onClick={() => setToogleNavbar(!toogleNavbar)}>
-          <img 
-            src={toogleNavbar ? hamburger_non_active : hamburger_active} alt="" />
+          {toogleNavbar ? <FiX size={29} className="text-fontColor" /> : <FiMenu size={29} className="text-fontColor" />}
+        </div>
+        <div className="hidden md:block text-fontColor text-sm">
+          <ul className="flex flex-row gap-4">
+            <li><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">Home</a></li>
+            <li><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">About</a></li>
+            <li><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">Portofolio</a></li>
+            <li><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">Contact</a></li>
+          </ul>
         </div>
       </nav>
       <div className={toogleNavbar ? "block" : "hidden" }>
-        <ul className="px-8">
-          <li className="py-2"><a href="#">Home</a></li>
-          <li className="py-2"><a href="#">About</a></li>
-          <li className="py-2"><a href="#">Portofolio</a></li>
-          <li className="py-2"><a href="#">Contact</a></li>
+        <ul className="px-8 bg-backgroundColor text-fontColor py-2 border-t-2 border-b-2 border-mainColor">
+          <li className="py-2"><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">Home</a></li>
+          <li className="py-2"><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">About</a></li>
+          <li className="py-2"><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">Portofolio</a></li>
+          <li className="py-2"><a className="hover:text-mainColor hover:border-b-2 border-mainColor" href="#">Contact</a></li>
         </ul>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '500px',
+      'md': '768px',
+      'lg': '1024px'
+    },
     extend: {
       colors: {
         'backgroundColor': '#21232f',
